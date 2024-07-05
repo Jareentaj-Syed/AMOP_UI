6178a9843d178a557ab27da94e55bef92b74a147
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-05 20:08:43 +0530

    remove users in header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -9,7 +9,6 @@ const Header: React.FC = () => {
   return (
     <div className="p-2 flex justify-between items-center header">
         <Image src="/amop-core.png" alt="AMOP Core Logo" width={150} height={40} className="logo" />
-      <h1 className="text-2xl font-bold">Users</h1>
       <div className="flex items-center space-x-4">
         <button className="p-2 rounded-full hover:bg-gray-200 hover:text-gray-800">
           <EnvelopeIcon className="w-6 h-6" />
