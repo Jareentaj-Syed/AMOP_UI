be0421b0fb5d34eb729f09d880a24a128bc2c50e
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-10 20:29:50 +0530

    update fill active-icon

--- src/app/components/sideNav.tsx
+++ src/app/components/sideNav.tsx
@@ -14,22 +14,22 @@ const SideNav: React.FC = () => {
   const navItems = [
     { href: '/', label: 'Users', icon: <HomeIcon className="w-4 h-4" /> },
     { href: '/partner-onboarding-form', label: 'Partner Onboarding form', icon: <ClipboardDocumentCheckIcon className="w-4 h-4" /> },
-    { 
-      label: 'SIM Management', 
+    {
+      label: 'SIM Management',
       icon: <WifiIcon className="w-4 h-4" />,
       subNav: [
         { href: '/sim-management/inventory', label: 'Inventory' },
         { href: '/sim-management/bulk-change', label: 'Bulk Change' },
         { href: '/sim-management/sim-order-form', label: 'SIM Order Form' },
         { href: '/sim-management/revenue-assurance', label: 'Revenue Assurance' },
-        
+
 
       ]
     },
     { href: '/device-management', label: 'Device Management', icon: <DevicePhoneMobileIcon className="w-4 h-4" /> },
     { href: '/optimization', label: 'Optimization', icon: <ChartBarIcon className="w-4 h-4" /> },
-    { 
-      label: 'People', 
+    {
+      label: 'People',
       icon: <UserGroupIcon className="w-4 h-4" />,
       subNav: [
         { href: '/people/rev_io_customers', label: 'Rev.Io Customers' },
@@ -58,13 +58,14 @@ const SideNav: React.FC = () => {
                   <>
                     <button
                       onClick={() => handleDropdownClick(item.label)}
-                      className={`flex items-center space-x-2 p-2 nav-link w-[100%] ${
-                        currentPath.startsWith(item.subNav[0].href.split('/')[1]) || openDropdown === item.label ? 'nav-active-link' : ''
-                      }`}
+                      className={`flex items-center space-x-2 p-2 nav-link w-[100%] ${currentPath.startsWith(item.subNav[0].href.split('/')[1]) || openDropdown === item.label ? 'nav-active-link' : ''
+                        }`}
                     >
-                      {item.icon}
+                      {React.cloneElement(item.icon as React.ReactElement, {
+                      className: `w-4 h-4 ${currentPath === item.href ? 'active-icon' : ''}`, 
+                    })}
                       <span>{item.label}</span>
-                      {openDropdown === item.label ? <ChevronDownIcon className="w-4 h-4 absolute "style={{ right: '20px' }}  /> : <ChevronRightIcon className="w-4 h-4 absolute"style={{ right: '20px' }}  />}
+                      {openDropdown === item.label ? <ChevronDownIcon className="w-4 h-4 absolute " style={{ right: '20px' }} /> : <ChevronRightIcon className="w-4 h-4 absolute" style={{ right: '20px' }} />}
                     </button>
                     {(openDropdown === item.label || currentPath.startsWith(item.subNav[0].href.split('/')[1])) && (
                       <ul className="pl-6 space-y-2 mt-2">
@@ -72,9 +73,8 @@ const SideNav: React.FC = () => {
                           <li key={subItem.href}>
                             <Link
                               href={subItem.href}
-                              className={`flex items-center space-x-2 p-2 nav-link ${
-                                currentPath === subItem.href ? 'nav-active-link' : ''
-                              }`}
+                              className={`flex items-center space-x-2 p-2 nav-link ${currentPath === subItem.href ? 'nav-active-link' : ''
+                                }`}
                             >
                               <span>{subItem.label}</span>
                             </Link>
@@ -86,16 +86,21 @@ const SideNav: React.FC = () => {
                 ) : (
                   <Link
                     href={item.href}
-                    className={`flex items-center space-x-2 p-2 nav-link ${
-                      currentPath === item.href || (item.href === '/' && (currentPath === '/' || currentPath === '/users/createUser'))
+                    passHref
+                    className={`flex items-center space-x-2 p-2 nav-link ${currentPath === item.href ||
+                        (item.href === '/' &&
+                          (currentPath === '/' || currentPath === '/users/createUser'))
                         ? 'nav-active-link'
                         : ''
-                    }`}
+                      }`}
                     onClick={() => handleDropdownClick(item.label)}
                   >
-                    {item.icon}
+                    {React.cloneElement(item.icon as React.ReactElement, {
+                      className: `w-4 h-4 ${currentPath === item.href ? 'active-icon' : ''}`, 
+                    })}
                     <span>{item.label}</span>
                   </Link>
+
                 )}
               </li>
             ))}
@@ -110,11 +115,12 @@ const SideNav: React.FC = () => {
                   <>
                     <button
                       onClick={() => handleDropdownClick(item.label)}
-                      className={`flex items-center justify-center pt-2 pb-2 rounded-md nav-link ${
-                        currentPath.startsWith(item.subNav[0].href.split('/')[1]) || openDropdown === item.label ? 'nav-active-link' : ''
-                      }`}
+                      className={`flex items-center justify-center pt-2 pb-2 rounded-md nav-link ${currentPath.startsWith(item.subNav[0].href.split('/')[1]) || openDropdown === item.label ? 'nav-active-link' : ''
+                        }`}
                     >
-                      {item.icon}
+                      {React.cloneElement(item.icon as React.ReactElement, {
+                      className: `w-4 h-4 ${currentPath === item.href ? 'active-icon' : ''}`, 
+                    })}
                       {openDropdown === item.label ? <ChevronDownIcon className="w-4 h-4" /> : <ChevronRightIcon className="w-4 h-4" />}
                     </button>
                     {(openDropdown === item.label || currentPath.startsWith(item.subNav[0].href.split('/')[1])) && (
@@ -123,9 +129,8 @@ const SideNav: React.FC = () => {
                           <li key={subItem.href}>
                             <Link
                               href={subItem.href}
-                              className={`flex items-center justify-center pt-2 pb-2 rounded-md nav-link ${
-                                currentPath === subItem.href ? 'nav-active-link' : ''
-                              }`}
+                              className={`flex items-center justify-center pt-2 pb-2 rounded-md nav-link ${currentPath === subItem.href ? 'nav-active-link' : ''
+                                }`}
                             >
                               <span>{subItem.label}</span>
                             </Link>
@@ -137,13 +142,14 @@ const SideNav: React.FC = () => {
                 ) : (
                   <Link
                     href={item.href}
-                    className={`flex items-center justify-center pt-2 pb-2 rounded-md nav-link ${
-                      currentPath === item.href || (item.href === '/' && (currentPath === '/' || currentPath === '/users/createUser'))
+                    className={`flex items-center justify-center pt-2 pb-2 rounded-md nav-link ${currentPath === item.href || (item.href === '/' && (currentPath === '/' || currentPath === '/users/createUser'))
                         ? 'nav-active-link'
                         : ''
-                    }`}
+                      }`}
                   >
-                    {item.icon}
+                    {React.cloneElement(item.icon as React.ReactElement, {
+                      className: `w-4 h-4 ${currentPath === item.href ? 'active-icon' : ''}`, 
+                    })}
                   </Link>
                 )}
               </li>
