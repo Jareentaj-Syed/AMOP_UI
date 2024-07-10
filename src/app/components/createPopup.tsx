0bdf172d5d07deb017a77522c0df4e69146dd373
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-10 19:54:25 +0530

    update people module

--- src/app/components/createPopup.tsx
+++ src/app/components/createPopup.tsx
@@ -25,8 +25,8 @@ const CreateModal: React.FC<CreateModalProps> = ({ isOpen, onClose, onSave, colu
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded shadow-lg w-3/4 max-w-4xl">
-        <h2 className="text-xl font-semibold mb-4">Create New Row</h2>
+      <div className="bg-white p-6 rounded shadow-lg w-3/4 max-w-4xl createPopup">
+        <h2 className="text-xl font-semibold mb-4">Add New Customer</h2>
         <div className="space-y-4">
           {columnNames.map((key) => (
             key !== 'api_state' && (
@@ -46,13 +46,13 @@ const CreateModal: React.FC<CreateModalProps> = ({ isOpen, onClose, onSave, colu
         <div className="flex justify-end space-x-4 mt-4">
           <button
             onClick={onClose}
-            className="px-4 py-2 rounded"
+            className="bg-gray-500 w-[80px] text-white px-4 py-2 rounded"
           >
             Cancel
           </button>
           <button
             onClick={handleSave}
-            className="px-4 py-2 rounded"
+            className="bg-blue-500 w-[80px] text-white px-4 py-2 rounded"
           >
             Save
           </button>
