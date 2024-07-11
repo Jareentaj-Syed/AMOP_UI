540d7e340a390c9bd8e5441e61f9735b588eba7f
Anjani Devi Akula <anjanidevi.a@algonox.com>
2024-07-11 15:15:48 +0530

    moduleconmappng

---
+++ src/app/constants/moduleiconmap.ts
@@ -0,0 +1,10 @@
+import { ClipboardDocumentCheckIcon, WifiIcon, DevicePhoneMobileIcon, ChartBarIcon, UserGroupIcon, CogIcon } from '@heroicons/react/24/outline'; 
+
+export const iconMap = {
+  'Partner Onboarding form': ClipboardDocumentCheckIcon,
+  'SIM Management': WifiIcon,
+  'Device Management': DevicePhoneMobileIcon,
+  'Optimization': ChartBarIcon,
+  'People': UserGroupIcon,
+  'Settings': CogIcon,
+};
