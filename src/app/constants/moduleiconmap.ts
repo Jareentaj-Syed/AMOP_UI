c53c3d5facef679abac59fa97e654e37e8ed9ab2
Anjani Devi Akula <anjanidevi.a@algonox.com>
2024-07-11 15:21:46 +0530

    moduledata

--- src/app/constants/moduleiconmap.ts
+++ src/app/constants/moduleiconmap.ts
@@ -8,3 +8,52 @@ export const iconMap = {
   'People': UserGroupIcon,
   'Settings': CogIcon,
 };
+
+export const moduleData = [
+    {
+      parent_module_name: 'Partner Onboarding form',
+      queue_order: 1,
+      children: [],
+    },
+    {
+      parent_module_name: 'SIM Management',
+      queue_order: 2,
+      children: [
+        { child_module_name: 'Inventory', queue_order: 1, sub_children: [] },
+        { child_module_name: 'Bulk Change', queue_order: 2, sub_children: [] },
+        { child_module_name: 'SIM Order Form', queue_order: 3, sub_children: [] },
+        { child_module_name: 'Revenue Assurance', queue_order: 4, sub_children: [] },
+      ],
+    },
+    {
+      parent_module_name: 'Device Management',
+      queue_order: 3,
+      children: [],
+    },
+    {
+      parent_module_name: 'Optimization',
+      queue_order: 4,
+      children: [],
+    },
+    {
+      parent_module_name: 'People',
+      queue_order: 5,
+      children: [
+        { child_module_name: 'Rev.Io Customers', queue_order: 1, sub_children: [] },
+        { child_module_name: 'Bandwidth Customers', queue_order: 2, sub_children: [] },
+        { child_module_name: 'NetSapiens Customers', queue_order: 3, sub_children: [] },
+        { child_module_name: 'E911 Customers', queue_order: 4, sub_children: [] },
+        { child_module_name: 'Customer Groups', queue_order: 5, sub_children: [] },
+        {
+          child_module_name: 'Users',
+          queue_order: 6,
+          sub_children: [],
+        },
+      ],
+    },
+    {
+      parent_module_name: 'Settings',
+      queue_order: 6,
+      children: [],
+    },
+  ];
