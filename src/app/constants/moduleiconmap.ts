338fa4c4c63906b25ecf6f5a53b36f5a0d5ac0f0
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-11 16:14:44 +0530

    update navitems format

--- src/app/constants/moduleiconmap.ts
+++ src/app/constants/moduleiconmap.ts
@@ -1,6 +1,6 @@
 import { ClipboardDocumentCheckIcon, WifiIcon, DevicePhoneMobileIcon, ChartBarIcon, UserGroupIcon, CogIcon } from '@heroicons/react/24/outline'; 
 
-export const iconMap = {
+export const moduleIconMap = {
   'Partner Onboarding form': ClipboardDocumentCheckIcon,
   'SIM Management': WifiIcon,
   'Device Management': DevicePhoneMobileIcon,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // app/page.tsx
 "use client"
 import React from 'react';
-import PartnerPartnerOnboardingForm from './partner-onboarding-form/page'
+import PartnerPartnerOnboardingForm from './partner_onboarding_form/page'
 
 const Page: React.FC = () => {
   return (
