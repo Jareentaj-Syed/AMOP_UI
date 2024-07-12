cd0fba899d2e2ea8dcc75aa7b6fe193df422d169
sindhuja-sid <sindhuja.karinki@algonox.com>
2024-07-12 13:04:01 +0530

    alignment for inventory

--- src/app/sim_management/inventory/page.tsx
+++ src/app/sim_management/inventory/page.tsx
@@ -8,20 +8,31 @@ import AdvancedFilter from './advanced-filter';
 const sim_management: React.FC = () => {
    const EXPORT = " Export";
    const searchPlaceholder = "Search"
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', gap: '10px', marginLeft:'20px',  marginTop:'20px' }}>
-      <AdvancedFilter/>
-      <Button
-              type="primary"
-              className="ml-2"
-              icon={<DownloadOutlined />}
-              size="large"
-              ghost
-            >
-              {EXPORT}
-       </Button>
-     
-       
+   return (
+    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', padding: '20px' }}>
+      <Input.Search
+        placeholder={searchPlaceholder}
+        className="w-full lg:w-auto"
+        allowClear
+        size="large"
+      />
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', gap: '10px' }}>
+        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+          <Button
+            type="primary"
+            className="ml-2"
+            icon={<DownloadOutlined />}
+            size="large"
+            ghost
+          >
+            {EXPORT}
+          </Button>
+          
+        </div>
+        <div style={{ marginTop: '10px' }}>
+          <AdvancedFilter />
+        </div>
+      </div>
     </div>
   );
 };
