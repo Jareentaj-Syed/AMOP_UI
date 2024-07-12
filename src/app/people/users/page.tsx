3da64ddd108d9b11c256d400564261c691b2a76c
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-12 19:54:23 +0530

    update scroll

--- src/app/people/users/page.tsx
+++ src/app/people/users/page.tsx
@@ -4,18 +4,14 @@ import React from 'react';
 import ListView from './listView/page';
 import ChartsPage from './charts/page';
 import { useRouter } from 'next/navigation';
-import { PlusIcon, ArrowDownTrayIcon, MagnifyingGlassIcon, FunnelIcon, AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline';
 
 const Page: React.FC = () => {
   const router = useRouter();
-  const handleCreateClick = () => {
-    router.push('users/createUser');
-  };
-
+ 
   return (
-    <div className="flex min-h-screen">
+    <div className="flex container">
       <div className="flex-1 flex flex-col">
-        <main className="p-2 flex-1">
+        <main className="p-2 flex">
           {/* <ChartsPage /> */}
         
         
