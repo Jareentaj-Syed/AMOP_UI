3c22577c01f2dfa630c37db470db37505df7a43d
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-16 18:15:26 +0530

    update partner_info submit

--- src/app/partner_onboarding_form/partner_info.tsx
+++ src/app/partner_onboarding_form/partner_info.tsx
@@ -11,7 +11,7 @@ const PartnerInfo: React.FC<PartnerInfo> = ({ onSubmit }) => {
   const [logoError, setLogoError] = useState<string | null>(null);
   const [activeElement, setActiveElement] = useState<string | null>(null);
   const [formValid, setFormValid] = useState<boolean>(false);
-  const [SubmitModalOpen, setSubmitModalOpen] = useState(false);
+  const [submitModalOpen, setSubmitModalOpen] = useState(false);
   const [partnerName, setPartnerName] = useState<string>('');
   const [subPartnerName, setSubPartnerName] = useState<string>('');
   const [emailList, setEmailList] = useState<string[]>([]);
@@ -27,7 +27,6 @@ const PartnerInfo: React.FC<PartnerInfo> = ({ onSubmit }) => {
       emailList.length > 0
     ) {
       setFormValid(true);
-      
     } else {
       setFormValid(false);
     }
@@ -35,24 +34,7 @@ const PartnerInfo: React.FC<PartnerInfo> = ({ onSubmit }) => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const file = logoFileRef.current?.files?.[0];
-    
-    if (file) {
-      const validTypes = ['image/png', 'image/jpeg'];
-      if (!validTypes.includes(file.type)) {
-        setLogoError('Only .png and .jpg files are allowed.');
-      } else {
-        const reader = new FileReader();
-        reader.onload = () => {
-          const logoUrl = reader.result as string;
-          setLogoUrl(logoUrl);
-          setSubmitModalOpen(true); // Open modal after logo is processed
-        };
-        reader.readAsDataURL(file);
-      }
-    } else {
-      setSubmitModalOpen(true); // Open modal without logo
-    }
+    setSubmitModalOpen(true);
   };
 
   const handleFocus = (elementName: string) => {
@@ -73,19 +55,48 @@ const PartnerInfo: React.FC<PartnerInfo> = ({ onSubmit }) => {
   };
 
   const confirmSubmit = () => {
-    // Perform submission logic here
-    setSubmitModalOpen(false); 
-    onSubmit()// Close modal after submission
+    const file = logoFileRef.current?.files?.[0];
+
+    if (file) {
+      const validTypes = ['image/png', 'image/jpeg'];
+      if (!validTypes.includes(file.type)) {
+        setLogoError('Only .png and .jpg files are allowed.');
+        setSubmitModalOpen(false);
+        return;
+      } else {
+        const reader = new FileReader();
+        reader.onload = () => {
+          const logoUrl = reader.result as string;
+          setLogoUrl(logoUrl);
+          resetForm();
+          setSubmitModalOpen(false);
+        };
+        reader.readAsDataURL(file);
+      }
+    } else {
+      resetForm();
+      setSubmitModalOpen(false);
+    }
+  };
+
+  const resetForm = () => {
+    setPartnerName('');
+    setSubPartnerName('');
+    setEmailList([]);
+    setLogoError(null);
+    if (logoFileRef.current) {
+      logoFileRef.current.value = '';
+    }
   };
 
   return (
     <div className="p-2">
-      <div className="mb-6 ">
+      <div className="mb-6">
         <h3 className="text-lg font-semibold mb-2 text-blue-500 bg-gray-200 pl-4 py-2">Partner Info</h3>
         <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-2 gap-4 mb-6">
             <div>
-              <label className={`block  ${activeElement === 'partnerName' ? 'text-blue-500' : 'text-gray-700'}`}>
+              <label className={`block ${activeElement === 'partnerName' ? 'text-blue-500' : 'text-gray-700'}`}>
                 Partner Name
               </label>
               <input
@@ -111,13 +122,13 @@ const PartnerInfo: React.FC<PartnerInfo> = ({ onSubmit }) => {
               />
             </div>
             <div>
-              <label className={`block  ${activeElement === 'emailIds' ?'text-blue-500' : 'text-gray-700'}`}>
+              <label className={`block ${activeElement === 'emailIds' ? 'text-blue-500' : 'text-gray-700'}`}>
                 Email ids
               </label>
               <div className="flex items-center">
                 <input
                   type="text"
-                  placeholder='Click on + to add email'
+                  placeholder="Click on + to add email"
                   className={`input flex-grow focus:border-sky-500 ${activeElement === 'emailIds' ? 'border-sky-500' : ''}`}
                   onFocus={() => handleFocus('emailIds')}
                   onBlur={handleBlur}
@@ -127,7 +138,7 @@ const PartnerInfo: React.FC<PartnerInfo> = ({ onSubmit }) => {
                 <button
                   type="button"
                   className="save-btn email-plus"
-                  onClick={() => setIsEmailModalOpen(true)}                
+                  onClick={() => setIsEmailModalOpen(true)}
                 >
                   +
                 </button>
@@ -167,10 +178,13 @@ const PartnerInfo: React.FC<PartnerInfo> = ({ onSubmit }) => {
         onRemoveEmail={handleRemoveEmail}
       />
       <Modal
-        title={<span style={{  fontWeight: 'bold' , fontSize:'16px' }}>Confirm Submission</span>}
-        open={SubmitModalOpen}
+        title={<span style={{ fontWeight: 'bold', fontSize: '16px' }}>Confirm Submission</span>}
+        open={submitModalOpen}
         onOk={confirmSubmit}
-        onCancel={() => setSubmitModalOpen(false)}
+        onCancel={() => {
+          resetForm();
+          setSubmitModalOpen(false);
+        }}
         centered
       >
         <p>Do you want to submit this Partner Info Form?</p>
