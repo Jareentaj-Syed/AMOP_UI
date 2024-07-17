b99bee48ab070766b38fb7aa50ef7a4080ed2b0c
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-17 13:18:31 +0530

    update css file

---
+++ src/app/components/css/dropdown.tsx
@@ -0,0 +1,61 @@
+export const NonEditableDropdownStyles = {
+    control: (base: any, state: any) => ({
+        ...base,
+        color: 'gray',
+        backgroundColor: '#F9FAFB',
+        fontSize: '16px',
+        width: '100%',
+        border: '1px solid #d1d5db',
+        borderRadius: '4px',
+        transition: 'border-color 0.2s ease, box-shadow 0.2s ease',
+        textAlign: 'left',
+        padding: '2px',
+    }),
+    singleValue: (provided: any) => ({
+        ...provided,
+        color: 'gray',
+    }),
+    // menu: (provided: any) => ({
+    //     ...provided,
+    //     backgroundColor: '#F9FAFB',
+    // }),
+    option: (provided: any, state: any) => ({
+        ...provided,
+        backgroundColor: state.isSelected ? '#BFDBFE' : '',
+        color: 'black',
+        ':hover': {
+        backgroundColor: '#BFDBFE',
+    },
+    }),
+};
+
+export const DropdownStyles = {
+    control: (base: any, state: any) => ({
+        ...base,
+        color: 'black',
+        backgroundColor: 'white',
+        fontSize: '16px',
+        width: '100%',
+        border: '1px solid #d1d5db',
+        borderRadius: '4px',
+        transition: 'border-color 0.2s ease, box-shadow 0.2s ease',
+        textAlign: 'left',
+        padding: '2px'
+    }),
+    singleValue: (provided: any) => ({
+        ...provided,
+        color: 'gray',
+    }),
+    // menu: (provided: any) => ({
+    //     ...provided,
+    //     backgroundColor: '#F9FAFB',
+    // }),
+    option: (provided: any, state: any) => ({
+        ...provided,
+        backgroundColor: state.isSelected ? '#BFDBFE' : '',
+        color: 'black',
+        ':hover': {
+        backgroundColor: '#BFDBFE',
+    },
+    }),
+};
