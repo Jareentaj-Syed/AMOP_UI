0f084c8ca8bb8ea22373665dbc092877c0316da5
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-23 14:52:45 +0530

    update width

--- src/app/components/password_reset.tsx
+++ src/app/components/password_reset.tsx
@@ -18,7 +18,7 @@ const PasswordReset: React.FC = () => {
   <Login />
 ) : (
   <div className="flex flex-col items-center justify-center h-screen">
-    <div className="w-[300px] h-[55px] mb-6">
+    <div className="w-[320px] h-[55px] mb-6">
       <Image
         src="/amop_logo_header.png"
         alt="AMOP Core Logo"
@@ -27,14 +27,14 @@ const PasswordReset: React.FC = () => {
         height={55}
       />
     </div>
-    <div className="flex flex-col items-start w-[300px] px-2 mb-6">
+    <div className="flex flex-col items-start w-[320px] pr-2 mb-6">
       <label className="text-[20px] text-left text-[#00C1F1] font-[700] mb-1">
         Reset Password
       </label>
       <Input
         placeholder="Email"
         style={{
-          width: '300px',
+          width: '320px',
           height: '35px',
           backgroundColor: '#F7F9FA',
           padding: '6px 12px',
