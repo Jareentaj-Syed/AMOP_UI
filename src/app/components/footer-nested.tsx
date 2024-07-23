087d0d75c27bece2f1d6b26abd845592e9fec1ce
Jareentaj-Syed <jareentaj.syed@algonox.com>
2024-07-23 12:05:47 +0530

    update footer styles

--- src/app/components/footer-nested.tsx
+++ src/app/components/footer-nested.tsx
@@ -3,8 +3,7 @@ import React from "react";
 export const Footer: React.FC = () => {
   return (
     <div
-      style={{ fontSize: "0.6rem" }}
-      className="tracking-wide text-center bottom-0 right-0 bg-white p-2 z-50"
+      className=" text-[11px] tracking-wide text-center font-[700] text-[#00C1F1] bottom-0 right-0 bg-white p-2 z-50"
     >
       COPYRIGHT &copy; 2018-2023 AMOP
     </div>
